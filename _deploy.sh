e446cddd9beef41ba6fe0e9c703c3fde3fb404bd
pat-s <patrick.schratz@gmail.com>
2017-03-05 23:03:24 +0100

    add .build & .deploy & travis.yml

---
+++ _deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dev" ] && exit 0
+
+git config --global user.email "patrick.schratz@gmail.com"
+git config --global user.name "pat-s"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git curso-output
+cd curso-output
+git rm -rf .
+cp -r ../public/* ./
+git add --all *
+git commit -m "Update curso-r" || true
+git push -q origin gh-pages
