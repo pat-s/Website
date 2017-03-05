f2a15fa46bb4efc881aec5ddc12cc018675c5fcd
pat-s <patrick.schratz@gmail.com>
2017-03-05 23:41:13 +0100

    update deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -3,12 +3,12 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "dev" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "travis" ] && exit 0
 
 git config --global user.email "patrick.schratz@gmail.com"
 git config --global user.name "pat-s"
 
-git clone -b gh-pages https://github.com/pat-s/pat-s.github.io.git curso-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git curso-output
 cd curso-output
 git rm -rf .
 cp -r ../public/* ./
