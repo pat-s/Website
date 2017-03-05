a0b879295cfa58b1257d1a6329130206e9847f90
pat-s <patrick.schratz@gmail.com>
2017-03-05 23:46:43 +0100

    update deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "travis" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "patrick.schratz@gmail.com"
 git config --global user.name "pat-s"
@@ -13,5 +13,5 @@ cd curso-output
 git rm -rf .
 cp -r ../public/* ./
 git add --all *
-git commit -m "Update curso-r" || true
+git commit -m "Update Website" || true
 git push -q origin gh-pages
